1b7b546245647de30b05be5632bbf458cb066834
Jami40 <jnjami40@gmail.com>
2026-09-05 15:01:33 +0600

    OOPS For MLOPS Started

---
+++ MLOPS_OOPS/oops.py
@@ -0,0 +1,10 @@
+class employee:
+
+    def __init__(self):
+        self.name = "John Doe"
+        self.age = 30
+        self.position = "Software Engineer"
+
+data=employee()
+
+print(data.position)
