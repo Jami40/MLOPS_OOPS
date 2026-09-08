af4905adbfc69a68515187485d4e71f50e9b64bc
Jami40 <jnjami40@gmail.com>
2026-09-08 12:29:01 +0600

    SignUp and SignIn added Fully Functional

--- MLOPS_OOPS/oops_proj.py
+++ MLOPS_OOPS/oops_proj.py
@@ -18,15 +18,35 @@ class chatbook:
                         """)
 
         if user_info=="1":
-            pass
+            self.signup()
         elif user_info=="2":
-            pass
+            self.signin()
         elif user_info=="3":
             pass
         elif user_info=="4":
             pass
         else:
             exit()
+    def signup(self):
+        self.username=input("Enter your username:")
+        self.password=input("Enter your password:")
+        print(f"Your account has been created successfully with username:{self.username} and password:{self.password}") 
+        print("/n")
+        self.menu()
+    def signin(self):
+        if self.username=="" and self.password=="":
+            print("You have not created an account yet. Please create an account first.")
+            self.menu()
+        else:
+            username=input("Enter your username:")
+            password=input("Enter your password:")
+            if username==self.username and password==self.password:
+                print(f"Welcome back {self.username}!")
+                self.loggedin=True
+                self.menu()
+            else:
+                print("Invalid username or password. Please try again.")
+                self.menu()
 
 
 obj=chatbook()
