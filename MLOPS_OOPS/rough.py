fa2bf525ce82a398b968129dd9238e2539ee741b
Jami40 <jnjami40@gmail.com>
2026-09-10 13:15:45 +0600

    Encaptulation static and protected and others oops method done

--- MLOPS_OOPS/rough.py
+++ MLOPS_OOPS/rough.py
@@ -3,3 +3,21 @@ lst=[1,2,3,5]
 lst.clear()
 
 print(lst)
+
+
+from oops_proj import chatbook
+
+user1=chatbook()
+# print(user1.get_name())
+# user1.set_name("Alice")
+# print(user1.get_name())
+
+# Using static method directly from class rather than obj
+print(user1.id)
+chatbook.set_id(10)
+
+user2 = chatbook()
+print(user2.id)
+
+user3 = chatbook()
+print(user3.id)
